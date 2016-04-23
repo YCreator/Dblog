715e5e64b6c86b0872fce3c1e198870fa13ec881
yzdong <yzd.dong@foxmail.com>
2016-04-23 17:57:46 +0800

    基础设施层模块

---
+++ blog-infra/src/main/java/com/dong/blog/infra/App.java
@@ -0,0 +1,13 @@
+package com.dong.blog.infra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
